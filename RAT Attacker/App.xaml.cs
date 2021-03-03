37ef271ed8bb467249b311ae9c622c94943e03ab
constanthinium <constanthinium@gmail.com>
2021-03-03 12:59:44 +0300

    Replace exception mbox with form

--- RAT Attacker/App.xaml.cs
+++ RAT Attacker/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Drawing;
+using System.Media;
+using System.Windows.Forms;
 using System.Windows.Threading;
 
 namespace RAT_Attacker
@@ -7,8 +10,31 @@ namespace RAT_Attacker
     {
         private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
         {
-            e.Handled = MessageBox.Show(e.Exception.ToString(), "Unhandled exception. Leave app running?",
-                MessageBoxButton.YesNo, MessageBoxImage.Error) == MessageBoxResult.Yes;
+            e.Handled = true;
+
+            var form = new Form
+            {
+                Width = 800,
+                Height = 450,
+                MinimumSize = new Size(400, 225),
+                Icon = new Icon(GetResourceStream(new Uri("pack://application:,,,/RAT Attacker;component/Icons/shell32_240.ico")).Stream),
+                Text = "Error!"
+            };
+
+            var textBox = new TextBox
+            {
+                ReadOnly = true,
+                Text = e.Exception.ToString(),
+                Dock = DockStyle.Fill,
+                Multiline = true,
+                ScrollBars = ScrollBars.Vertical,
+                Font = new Font(new FontFamily("Consolas"), 12)
+            };
+
+            form.Controls.Add(textBox);
+            form.Show();
+            SystemSounds.Hand.Play();
+            textBox.SelectionLength = 0;
         }
     }
 }
